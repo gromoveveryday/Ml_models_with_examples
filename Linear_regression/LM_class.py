5de43eb396195eae272604cf8e2264e8f34ec455
Gromov Ilya <gromov_every_day@mail.ru>
2025-01-18 16:21:17 +0300

    Fix LM and Added Lasso/Ridge regressions examples + classes

--- Linear_regression/LM_class.py
+++ Linear_regression/LM_class.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import category_encoders as ce
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -7,16 +6,26 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # Так называемый класс
 class DataProcessing:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data, target_column, random_state, test_size, shuffle,
+                 fit_intercept, copy_X, n_jobs, positive):
+        self.data = data # объект DataFrame
+        self.target_column = target_column # целевой столбец
+        self.random_state = random_state # int
+        self.test_size = test_size # float
+        self.shuffle = shuffle # True/False
+        self.fit_intercept = fit_intercept # True/False
+        self.copy_X = copy_X # True/False
+        self.n_jobs = n_jobs # None/int
+        self.positive = positive # True/False
+
 
     # Так называемая очистка данных от пустых строк
     def clean_data_and_encoding(self):
         for index, row in self.data.iterrows():
             if row.isnull().sum() > 0:
                 self.data.drop(index, axis=0, inplace=True)
-
-        # Так называемое преобразование категориальных переменных
+        
+        # Так называемое горячее кодирование
         categorical_columns = []
         for column in self.data:
             if self.data[column].dtype == 'object':
@@ -24,38 +33,38 @@ class DataProcessing:
 
         encoder = ce.OneHotEncoder(cols=categorical_columns)
         encoded_data = encoder.fit_transform(self.data)
-        self.encoded_data = pd.DataFrame(encoded_data, columns=encoder.get_feature_names())
+        self.encoded_data = pd.DataFrame(encoded_data)
+
 
     # Так называемая кросс-валидация
-    def test_train_validation(self, target_column, random_state, test_size, shuffle):
-        X = self.encoded_data.drop([target_column], axis = 1)
-        y = self.encoded_data[target_column].values
+    def test_train_validation(self):
+        self.X = self.encoded_data.drop([self.target_column], axis = 1)
+        self.y = self.data[self.target_column].values
         
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y , 
-                                   random_state=random_state,  
-                                   test_size=test_size,  
-                                   shuffle=shuffle) 
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y, 
+                                   random_state=self.random_state,  
+                                   test_size=self.test_size,  
+                                   shuffle=self.shuffle) 
     
     
     # Так называемое обучение модели
-    def train_linear_regression(self, fit_intercept, copy_X, n_jobs, positive):
+    def train_linear_regression(self):
       
-        linear_model = LinearRegression(
-             fit_intercept=fit_intercept, # ключение 'b0' в модель (постоянный член)
-             copy_X=copy_X, # копирование данных на случай их перезаписи
-             n_jobs=n_jobs, # количество одновременно задействованных ядер
-             positive=positive # убирает неположительные коэффициенты из модели
+        self.linear_model = LinearRegression(
+             fit_intercept=self.fit_intercept, # ключение 'b0' в модель (постоянный член)
+             copy_X=self.copy_X, # копирование данных на случай их перезаписи
+             n_jobs=self.n_jobs, # количество одновременно задействованных ядер
+             positive=self.positive # убирает неположительные коэффициенты из модели
              )
-        linear_model.fit(self.X_train, self.y_train)
-        return model
-    
+        self.linear_model.fit(self.X_train, self.y_train)
+
     # Так называемый расчет метрик качества регресии
-    def evaluate_coefficients_and_mse(self, model):
-        coefficients = model.coef_
-        mse_train = mean_squared_error(y_true=self.y_train, y_pred=model.predict(self.X_train))
-        mse_test = mean_squared_error(y_true=self.y_test, y_pred=model.predict(self.X_test))
-        r_squared_train = r2_score(y_true=self.y_train, y_pred=model.predict(self.X_train))
-        r_squared_test = r2_score(_true=self.y_test, y_pred=model.predict(self.X_test))
+    def evaluate_coefficients_and_mse(self):
+        coefficients = self.linear_model.coef_
+        mse_train = mean_squared_error(y_true=self.y_train, y_pred=self.linear_model.predict(self.X_train))
+        mse_test = mean_squared_error(y_true=self.y_test, y_pred=self.linear_model.predict(self.X_test))
+        r_squared_train = r2_score(y_true=self.y_train, y_pred=self.linear_model.predict(self.X_train))
+        r_squared_test = r2_score(y_true=self.y_test, y_pred=self.linear_model.predict(self.X_test))
 
         print("Коэффициенты модели:")
         print(coefficients)
---
+++ Ridge/ridge.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import category_encoders as ce
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Так называемый класс
+class DataProcessing:
+    def __init__(self, data, target_column, random_state_val, alpha,
+                 tol, solver, random_state_ridge, test_size,
+                 fit_intercept, copy_X, max_iter, positive, shuffle):
+        self.data = data # объект DataFrame
+        self.target_column = target_column # целевой столбец
+        self.random_state_val = random_state_val # int (для валидации)
+        self.test_size = test_size
+        self.alpha = alpha # float
+        self.tol = tol # float
+        self.solver = solver # «auto», «svd», «cholesky», «lsqr», «sparse_cg», «sag», «saga» 
+        self.fit_intercept = fit_intercept # True/False
+        self.copy_X = copy_X # True/False
+        self.max_iter = max_iter # int
+        self.positive = positive # True/False
+        self.random_state_ridge = random_state_ridge # int (Ridge)
+        self.shuffle = shuffle # True/False
+
+
+    # Так называемая очистка данных от пустых строк
+    def clean_data_and_encoding(self):
+        for index, row in self.data.iterrows():
+            if row.isnull().sum() > 0:
+                self.data.drop(index, axis=0, inplace=True)
+        
+        # Так называемое горячее кодирование
+        categorical_columns = []
+        for column in self.data:
+            if self.data[column].dtype == 'object':
+                categorical_columns.append(column)
+
+        encoder = ce.OneHotEncoder(cols=categorical_columns)
+        encoded_data = encoder.fit_transform(self.data)
+        self.encoded_data = pd.DataFrame(encoded_data)
+
+
+    # Так называемая кросс-валидация
+    def test_train_validation(self):
+        self.X = self.encoded_data.drop([self.target_column], axis = 1)
+        self.y = self.data[self.target_column].values
+        
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y, 
+                                   random_state=self.random_state_val,  
+                                   test_size=self.test_size,  
+                                   shuffle=self.shuffle) 
+    
+    
+    # Так называемое обучение модели
+    def train_linear_regression_ridge(self):
+      
+        self.linear_model_ridge = Ridge(
+            alpha=self.alpha, # коэффициент регуляризации 
+            fit_intercept=self.fit_intercept, # ключение 'b0' в модель (постоянный член)
+            copy_X=self.copy_X, # копирование данных на случай их перезаписи
+            max_iter=self.max_iter, # максимальное количество итераций
+            tol=self.tol, # порог остановки алгоритма, если изменение функции потень стоновится меньше tol
+            solver=self.solver, # алгоритмы решения
+            positive=self.positive, # убирает неположительные коэффициенты из модели
+            random_state=self.random_state_ridge, # начальное значение генератора случайных числе
+             )
+        
+        self.linear_model_ridge.fit(self.X_train, self.y_train)
+
+    # Так называемый расчет метрик качества регресии
+    def evaluate_coefficients_and_mse(self):
+        coefficients = self.linear_model_ridge.coef_
+        mse_train = mean_squared_error(y_true=self.y_train, y_pred=self.linear_model_ridge.predict(self.X_train))
+        mse_test = mean_squared_error(y_true=self.y_test, y_pred=self.linear_model_ridge.predict(self.X_test))
+        r_squared_train = r2_score(y_true=self.y_train, y_pred=self.linear_model_ridge.predict(self.X_train))
+        r_squared_test = r2_score(y_true=self.y_test, y_pred=self.linear_model_ridge.predict(self.X_test))
+
+        print("Коэффициенты модели:")
+        print(coefficients)
+        print("MSE_train:", mse_train)
+        print("MSE_test::", mse_test)
+        print("R^2_train:", r_squared_train)
+        print("R^2_test:", r_squared_test)
